636397426d09b54214a0f0b64e0f44467c8c8e59
zwpdbh <hyperion_z@outlook.com>
2016-05-04 00:50:51 +1200

    < add splash screen.

---
+++ app/src/main/java/com/otago/zw/housefinder/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.otago.zw.housefinder;
+
+/**
+ * Created by zw on 5/4/16.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by vamsikrishna on 12-Feb-15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MapsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
