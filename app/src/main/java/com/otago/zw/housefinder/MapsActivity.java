15276a619e00b908ca4015761e14df2ec45ca689
zwpdbh <hyperion_z@outlook.com>
2016-05-04 11:12:11 +1200

    < Changed my map to use map fragment, not broken. >

--- app/src/main/java/com/otago/zw/housefinder/MapsActivity.java
+++ app/src/main/java/com/otago/zw/housefinder/MapsActivity.java
@@ -32,181 +32,178 @@ import android.widget.ListView;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
-        GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMarkerClickListener,
-        GoogleMap.OnInfoWindowClickListener {
+public class MapsActivity extends AppCompatActivity {
 
-    private GoogleMap mMap;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mCurrentLocation;
-
-    private String[] drawrerItems = {"Setting", "Saved Houses"};
-    private ListView mDrawerList;
-    private DrawerLayout mDrawerLayout;
+//    private GoogleMap mMap;
+//    private GoogleApiClient mGoogleApiClient;
+//    private Location mCurrentLocation;
+//
+//    private String[] drawrerItems = {"Setting", "Saved Houses"};
+//    private ListView mDrawerList;
+//    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                drawrerItems
-        );
-
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
+//        mapFragment.getMapAsync(this);
+//
+//        if (mGoogleApiClient == null) {
+//            mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+//                    .addOnConnectionFailedListener(this)
+//                    .addApi(LocationServices.API)
+//                    .build();
+//        }
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                drawrerItems
+//        );
+//
+//        mDrawerList.setAdapter(adapter);
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
-    private void selectItem(int position) {
-        if (position == 0) {
-            Intent settingIntent = new Intent(this, SettingActivity.class);
-            startActivity(settingIntent);
-        } else if (position == 1) {
-            Intent intent = new Intent(this, SavedHouse.class);
-            startActivity(intent);
-        }
-    }
-
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_setting:
-                Intent settingIntent = new Intent(this, SettingActivity.class);
-                startActivity(settingIntent);
-                return true;
-            case R.id.add_house_info:
-                Intent addHouseInfoIntent = new Intent(this, AddHouseActivity.class);
-                startActivity(addHouseInfoIntent);
-                return true;
-            case R.id.Otago_about:
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://www.otago.ac.nz"));
-                startActivity(i);
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        System.out.println("Connected");
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            System.out.println("sett my location:\n" + mCurrentLocation.getLatitude() + "\n" + mCurrentLocation.getLongitude());
-        } else {
-            // Show rationale and request permission.
-            System.out.println("Location Permission Failed");
-            System.out.println("Give me permission: Location");
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        System.out.println("onConnectionSuspended:" + i);
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        System.out.println(connectionResult.toString());
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-        // add listener
-        mMap.setOnMapClickListener(this);
-        mMap.setOnMapLongClickListener(this);
-        mMap.setOnMarkerClickListener(this);
-        mMap.setOnInfoWindowClickListener(this);
-
-        // Add a marker in Sydney and move the camera
-
-
-        if (mCurrentLocation != null) {
-            LatLng position = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-        } else {
-            LatLng sydney = new LatLng(-34, 151);
-            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney")).setSnippet("Test");
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
-
-    }
-
-    @Override
-    public void onMapClick(LatLng point) {
-        System.out.println(point.latitude + " : " + point.longitude);
-    }
-
-    @Override
-    public void onMapLongClick(LatLng point) {
-        LatLng newPosition = new LatLng(point.latitude, point.longitude);
-        mMap.addMarker(new MarkerOptions().position(newPosition).title("Added new position").draggable(true));
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        System.out.println(marker.getTitle());
-        return false;
-    }
-
-    @Override
-    public void onInfoWindowClick(Marker marker) {
-        System.out.println(marker.getTitle());
-        Intent intent = new Intent(this, HouseDetailActivity.class);
-        intent.putExtra(HouseDetailActivity.HOUSE_ID, marker.getTitle());
-        startActivity(intent);
-    }
+//    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            selectItem(position);
+//        }
+//    }
+//
+//    private void selectItem(int position) {
+//        if (position == 0) {
+//            Intent settingIntent = new Intent(this, SettingActivity.class);
+//            startActivity(settingIntent);
+//        } else if (position == 1) {
+//            Intent intent = new Intent(this, SavedHouse.class);
+//            startActivity(intent);
+//        }
+//    }
+//
+//    protected void onStart() {
+//        mGoogleApiClient.connect();
+//        super.onStart();
+//    }
+//
+//    protected void onStop() {
+//        mGoogleApiClient.disconnect();
+//        super.onStop();
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.menu_setting:
+//                Intent settingIntent = new Intent(this, SettingActivity.class);
+//                startActivity(settingIntent);
+//                return true;
+//            case R.id.add_house_info:
+//                Intent addHouseInfoIntent = new Intent(this, AddHouseActivity.class);
+//                startActivity(addHouseInfoIntent);
+//                return true;
+//            case R.id.Otago_about:
+//                Intent i = new Intent(Intent.ACTION_VIEW);
+//                i.setData(Uri.parse("http://www.otago.ac.nz"));
+//                startActivity(i);
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+//
+//    @Override
+//    public void onConnected(@Nullable Bundle bundle) {
+//        System.out.println("Connected");
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            mMap.setMyLocationEnabled(true);
+//            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//            System.out.println("sett my location:\n" + mCurrentLocation.getLatitude() + "\n" + mCurrentLocation.getLongitude());
+//        } else {
+//            // Show rationale and request permission.
+//            System.out.println("Location Permission Failed");
+//            System.out.println("Give me permission: Location");
+//        }
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//        System.out.println("onConnectionSuspended:" + i);
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//        System.out.println(connectionResult.toString());
+//    }
+//
+//    /**
+//     * Manipulates the map once available.
+//     * This callback is triggered when the map is ready to be used.
+//     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+//     * we just add a marker near Sydney, Australia.
+//     * If Google Play services is not installed on the device, the user will be prompted to install
+//     * it inside the SupportMapFragment. This method will only be triggered once the user has
+//     * installed Google Play services and returned to the app.
+//     */
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+//
+//        // add listener
+//        mMap.setOnMapClickListener(this);
+//        mMap.setOnMapLongClickListener(this);
+//        mMap.setOnMarkerClickListener(this);
+//        mMap.setOnInfoWindowClickListener(this);
+//
+//        // Add a marker in Sydney and move the camera
+//
+//
+//        if (mCurrentLocation != null) {
+//            LatLng position = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+//        } else {
+//            LatLng sydney = new LatLng(-34, 151);
+//            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney")).setSnippet("Test");
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onMapClick(LatLng point) {
+//        System.out.println(point.latitude + " : " + point.longitude);
+//    }
+//
+//    @Override
+//    public void onMapLongClick(LatLng point) {
+//        LatLng newPosition = new LatLng(point.latitude, point.longitude);
+//        mMap.addMarker(new MarkerOptions().position(newPosition).title("Added new position").draggable(true));
+//    }
+//
+//    @Override
+//    public boolean onMarkerClick(Marker marker) {
+//        System.out.println(marker.getTitle());
+//        return false;
+//    }
+//
+//    @Override
+//    public void onInfoWindowClick(Marker marker) {
+//        System.out.println(marker.getTitle());
+//        Intent intent = new Intent(this, HouseDetailActivity.class);
+//        intent.putExtra(HouseDetailActivity.HOUSE_ID, marker.getTitle());
+//        startActivity(intent);
+//    }
 }
