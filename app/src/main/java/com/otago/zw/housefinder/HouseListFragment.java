f9307f01ae1ee10b383965f477c0b636d5d89680
zwpdbh <hyperion_z@outlook.com>
2016-05-03 20:08:27 +1200

    < due to name conflict, change to house_list_fragment

--- app/src/main/java/com/otago/zw/housefinder/HouseListFragment.java
+++ app/src/main/java/com/otago/zw/housefinder/HouseListFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFragment extends Fragment {
+public class HouseListFragment extends Fragment {
 
 
-    public ListFragment() {
+    public HouseListFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.fragment_house_list, container, false);
     }
 
 }
