2aa66c2e3b8116b4cfb18db92c688fecc32237fc
zwpdbh <hyperion_z@outlook.com>
2016-05-04 14:19:05 +1200

    > need change saved_house from activity to fragment

--- app/src/main/java/com/otago/zw/housefinder/MapsActivity.java
+++ app/src/main/java/com/otago/zw/housefinder/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.otago.zw.housefinder;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,7 +15,7 @@ import android.widget.ListView;
 
 public class MapsActivity extends AppCompatActivity {
 
-    private String[] drawrerItems = {"Setting", "Saved Houses"};
+    private String[] drawrerItems = {"Home", "Setting", "Saved Houses"};
     private ListView mDrawerList;
     private DrawerLayout mDrawerLayout;
 
@@ -43,13 +46,31 @@ public class MapsActivity extends AppCompatActivity {
     }
 
     private void selectItem(int position) {
-        if (position == 0) {
-            Intent settingIntent = new Intent(this, SettingActivity.class);
-            startActivity(settingIntent);
-        } else if (position == 1) {
-            Intent intent = new Intent(this, SavedHouse.class);
-            startActivity(intent);
+//        if (position == 0) {
+//            Intent settingIntent = new Intent(this, SettingActivity.class);
+//            startActivity(settingIntent);
+//        } else if (position == 1) {
+//            Intent intent = new Intent(this, SavedHouse.class);
+//            startActivity(intent);
+//        }
+
+        Fragment fragment;
+        switch (position) {
+            case 0:
+                fragment = new MapFragment();
+                break;
+            case 1:
+                fragment = new SettingFragment();
+                break;
+            default:
+                fragment = new MapFragment();
         }
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.home_screen_container,fragment);
+        ft.addToBackStack(null);
+        ft.setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
 
 }
