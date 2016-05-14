5410d0cd8b333af3eb0f8ad48b923711b8ff1dfc
zwpdbh <hyperion_z@outlook.com>
2016-05-14 16:00:07 +1200

    < finished the code for db helper

--- app/src/main/java/com/otago/zw/housefinder/DatabaseHelper.java
+++ app/src/main/java/com/otago/zw/housefinder/DatabaseHelper.java
@@ -4,6 +4,7 @@ package com.otago.zw.housefinder;
  * Created by zw on 5/13/16.
  */
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -19,11 +20,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE HOUSE (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "NAME TEXT, "
+            + "DESCRIPTION TEXT, "
+            + "LATITUDE REAL, "
+            + "LONGITUDE REAL, "
+            + "IMAGE_RESOURCE_ID INTEGER);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    private static void insertHouse(SQLiteDatabase db, String name, String description, int latitude, int longitude, int imageResourceID) {
+        ContentValues houseValue = new ContentValues();
+        houseValue.put("NAME", name);
+        houseValue.put("DESCRIPTION", description);
+        houseValue.put("LATITUDE", latitude);
+        houseValue.put("LONGITUDE", longitude);
+        houseValue.put("IMAGE_RESOURCE_ID", imageResourceID);
+        db.insert("HOUSE", null, houseValue);
     }
 }
