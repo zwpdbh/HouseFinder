8a83cf85e809c4fcf98334a7c6f7ce759d6fbdbb
zwpdbh <hyperion_z@outlook.com>
2016-05-15 21:40:59 +1200

    <>It can save the description > when save a new one, direct to new intent

--- app/src/main/java/com/otago/zw/housefinder/AddHouseActivity.java
+++ app/src/main/java/com/otago/zw/housefinder/AddHouseActivity.java
@@ -3,13 +3,20 @@ package com.otago.zw.housefinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.otago.zw.housefinder.database.DatabaseHelper;
+
+import org.w3c.dom.Text;
 
 public class AddHouseActivity extends AppCompatActivity {
     public static final String LATITUDE = "House_latitude";
     public static final String LONGITUDE = "House_longitude";
+
+    private Button mButton;
 
 
     @Override
@@ -25,5 +32,38 @@ public class AddHouseActivity extends AppCompatActivity {
         System.out.println("Get latLng: " + latLng.latitude + "<>" +latLng.longitude);
         latitudeView.setText(latLng.latitude+"");
         longitudeView.setText(latLng.longitude+"");
+
+
+        mButton = (Button) findViewById(R.id.save_house_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HouseInfo houseInfo = HouseInfo.get(v.getContext());
+                House house = new House();
+
+                TextView latitudeView = (TextView)findViewById(R.id.latitude);
+                double latitude = Double.parseDouble(latitudeView.getText().toString());
+
+                TextView longitudeView = (TextView)findViewById(R.id.longitude);
+                double longitude = Double.parseDouble(longitudeView.getText().toString());
+
+                TextView priceView = (TextView) findViewById(R.id.house_price);
+                int price = Integer.parseInt(priceView.getText().toString());
+
+                TextView descriptionView = (TextView) findViewById(R.id.house_description);
+                String description = descriptionView.getText().toString();
+
+                TextView addressView = (TextView) findViewById(R.id.house_address);
+                String address = addressView.getText().toString();
+
+                house.setDescription(description);
+                house.setPrice(price);
+                house.setAddress(address);
+                house.setLatitude(latitude);
+                house.setLongitude(longitude);
+
+                houseInfo.addHouse(house);
+            }
+        });
     }
 }
